bcfdaa6a1798da2aef4d902186d6e6e6b45fef2c
wrench <amichelangelo@outlook.com>
2026-06-15 01:37:49 +0800

    chore(file):move keymap.lua to core folder and move plugin.lua to plugin folder and rename plugin.lua to nvim-tree.lua

--- init.lua
+++ init.lua
@@ -2,11 +2,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-require("keymap")
-require("plugin")
+require("core.keymap")
+--require("plugin")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
-vim.opt.clipboard ="unnamedplus"
+--vim.opt.clipboard ="unnamedplus"
 
